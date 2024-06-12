cccbb5719469b56e5431b271d6bd01db308af466
Mohd Saqib <mohdsaqibmalik43@gmail.com>
2024-06-12 17:36:07 +0530

    Modified deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,3 +37,4 @@ set :keep_releases, 3
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+set :passenger_restart_with_touch, true
