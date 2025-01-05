0a000b2ebf64877e066c4eb58e14bc6e8eeeeb8a
Mohd Saqib <mohdsaqibmalik43@gmail.com>
2025-01-06 00:01:56 +0530

    Added valid response in user_interests controller

--- app/controllers/api/v1/user_interests_controller.rb
+++ app/controllers/api/v1/user_interests_controller.rb
@@ -4,6 +4,12 @@ class Api::V1::UserInterestsController < Api::V1::BaseController
       params[:interests_ids].each do |interest_id|
         current_user.user_interests.create(interest_id: interest_id)
       end
+
+      status = true
+    else
+      status = false
     end
+
+    render json: { status: status }, status: status ? 200 : 422
   end
 end
